39980d6ffd14a06684e663eb573c80f28cc0c40d
Will Adkins <wkadki01@gmail.com>
2018-08-06 17:35:34 -0400

    init commit

---
+++ app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import './App.css'
+
+const App = () => (
+  <BrowserRouter>
+    <Switch />
+  </BrowserRouter>
+)
+
+export default App
