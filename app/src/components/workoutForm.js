d640f0b2ac9c277688e8bcfbf47ee59954b2677c
Will Adkins <wkadki01@gmail.com>
2018-08-23 10:31:57 -0400

    DateTimePicker fixed onChange function; properly updates relevant state.

--- app/src/components/workoutForm.js
+++ app/src/components/workoutForm.js
@@ -118,7 +118,7 @@ const WorkoutForm = props => {
                 <DateTimePicker
                   label="Date"
                   value={new Date(dateTime)}
-                  onChange={date => onChange(date.toISOString())}
+                  onChange={date => onChange('dateTime', date.toISOString())}
                   disableFuture
                 />
               </div>
